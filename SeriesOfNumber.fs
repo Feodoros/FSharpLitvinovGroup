53065687770685994ab78ed8d95e9b0351a65954
Feodoros <fzhilkin@mail.ru>
2019-02-24 21:37:51 +0300

    Commit. v1.1.2

--- SeriesOfNumber.fs
+++ SeriesOfNumber.fs
@@ -1,3 +1,5 @@
+open System
+
 //Additional Methods
 let reverse list =
     let rec loop list acc =
@@ -6,33 +8,32 @@ let reverse list =
         | head::tail -> loop tail (head::acc)
     loop list []
 
-let rec MathPow Number Power =
+let rec mathPow Number Power =
     if Power = 1 then Number
-    else Number * MathPow Number (Power - 1)
+    else Number * mathPow Number (Power - 1)
 
 let rec readInteger() = 
-    match System.Int32.TryParse(System.Console.ReadLine()) with
-    | false, _ -> printfn "Waiting for integer number."; readInteger()
+    match Int32.TryParse(Console.ReadLine()) with
+    | false, _ -> printfn "Waiting for integer number."
+                  readInteger()
     | _, x -> x 
 
 
 //Main programm
-let PowersSeriesOfNum OurNumber StartPower NumberOfMembers  =
-    let rec loop StartPower NumberOfMembers (list:list<int>) acc =
+let powersSeriesOfTwo startPower numberOfMembers  =
+    let rec loop startPower numberOfMembers (list:list<int>) acc =
         match list.Length with
-        | index when index = NumberOfMembers + 1 -> reverse list
-        | 0 -> loop StartPower NumberOfMembers (MathPow OurNumber (list.Length + StartPower)::list) (MathPow OurNumber (list.Length + StartPower))
-        | _ -> loop StartPower NumberOfMembers (OurNumber * acc::list) (OurNumber * acc)
-    loop StartPower (NumberOfMembers - 1) [] OurNumber
+        | index when index = numberOfMembers + 1 -> reverse list
+        | 0 -> loop startPower numberOfMembers (mathPow 2 (list.Length + startPower)::list) (mathPow 2 (list.Length + startPower))
+        | _ -> loop startPower numberOfMembers (2 * acc::list) (2 * acc)
+    loop startPower (numberOfMembers - 1) [] 2
         
 
 printf("Start with power ")
-let PowerStart = readInteger()
+let powerStart = readInteger()
 
 printf("Number of members ")
-let NumberMembers = readInteger()
+let numberMembers = readInteger()
 
-printf("Number to make series ")
-let OurNumber = readInteger()
 
-let Answer = PowersSeriesOfNum OurNumber PowerStart NumberMembers 
+let answer = powersSeriesOfTwo powerStart numberMembers 

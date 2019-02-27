0c5c64dd93e7c8bf5aee42dc042a41f15e4937e9
Feodoros <fzhilkin@mail.ru>
2019-02-27 15:57:06 +0300

    Initial commit.

---
+++ Palindrome.fs
@@ -0,0 +1,7 @@
+open System
+
+let isPalindrome (s: string) = String.Compare(s, String(Array.rev (s.ToCharArray())), false) = 0 
+
+let answer = isPalindrome "FSSF"
+
+printfn "String is palindrome: %b" <| answer

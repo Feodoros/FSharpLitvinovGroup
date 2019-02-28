647fba34611d69a69efe9bae2d2ef711b99b391f
Feodoros <fzhilkin@mail.ru>
2019-02-28 15:52:00 +0300

    Solution files

---
+++ Task21/ConsoleApp1/firstAppereance_tests.fs
@@ -0,0 +1,19 @@
+﻿module tests
+
+open NUnit.Framework
+open FsUnit
+open logic
+
+
+
+[<Test>]
+let ``When getting sum of multiples of 3 and 5 to a max number of 10 it should return a sum of 23`` () =
+    firstAppearance [1..10] 5 |> should equal (Some(4))
+
+[<Test>]
+let ``asd`` () =
+    firstAppearance [1..3] 4 |> should equal (None)
+
+[<Test>]
+let ``asdd`` () =
+    firstAppearance [] 5 |> should equal None
--- Task21/Task21/firstAppereance.fs
+++ Task21/Task21/firstAppereance.fs
@@ -1,3 +1,5 @@
+﻿module logic
+
 let firstAppearance list number = 
     let rec loop list index =
         match list with
@@ -6,7 +8,7 @@ let firstAppearance list number =
         |_::_ -> loop (List.tail list) (index + 1)
     loop list 0
 
-let num = 7
+let num = 5
 let list = [1..10]
 
 let answer = firstAppearance list num

5802d9071d64a0ced039fada00ceaa2f2c459dc4
Feodoros <fzhilkin@mail.ru>
2019-02-28 16:27:16 +0300

    Itial logic

--- Task2.2/Task2.2/palindrome.fs
+++ Task2.2/Task2.2/palindrome.fs
@@ -1,1 +1,9 @@
 ﻿module logic 
+
+    open System
+
+    let isPalindrome (s: string) = String.Compare(s, String(Array.rev (s.ToCharArray())), false) = 0 
+
+    let answer = isPalindrome "FSSF"
+
+    printfn "String is palindrome: %b" <| answer 

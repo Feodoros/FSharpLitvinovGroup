4f0cc6666d38dd817a2104bdd6c8a207755c067d
Feodoros <fzhilkin@mail.ru>
2019-03-01 03:44:46 +0300

    Update tests

--- Task2.3/mergeSort_tests/mergeSort_tests.fs
+++ Task2.3/mergeSort_tests/mergeSort_tests.fs
@@ -5,13 +5,13 @@
     open logic 
 
     [<Test>]
-    let ``MergeSort 1 with list [3;1;2] should give out [1;2;3]`` () =
-        sortMerge [3;1;2] |> should equal [1;2;3]
+    let ``MergeSort 1 with list [3; 1; 2] should give out [1; 2; 3]`` () =
+        checkListAndStartSorting [3; 1; 2] |> should equal [1; 2; 3]
 
     [<Test>]
-    let ``MergeSort 2 with negative num should give out [-9;-1;0]`` () =
-        sortMerge [0;-9;-1] |> should equal [-9;-1;0]
+    let ``MergeSort 2 with negative num should give out [-9; -1; 0]`` () =
+        checkListAndStartSorting [0;-9;-1] |> should equal [-9; -1; 0]
 
     [<Test>]
     let ``MergeSort 3 with empty list should give out []`` () =
-        sortMerge [] |> should equal [] 
+        checkListAndStartSorting [] |> should equal [] 

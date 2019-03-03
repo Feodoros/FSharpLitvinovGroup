c4f9b94181d8cc69d5278a2f6a9be94fa444d691
Feodoros <fzhilkin@mail.ru>
2019-03-04 02:44:57 +0300

    Initial commit.

---
+++ Task3.1/Task3.1/evenNumInList.fs
@@ -0,0 +1,10 @@
+﻿module logic  
+
+    let evenNumWithMap list = 
+        List.length list - List.sum (List.map (fun x -> abs (x % 2)) list)
+
+    let evenNumWithFilter list =
+        List.length (List.filter (fun x -> x % 2 = 0) list)
+
+    let evenNumWithFold list =
+        List.fold (fun acc x -> acc - abs (x % 2)) (List.length list) list
---
+++ Task3.1/Task3.1_test/evenNumInList_tests.fs
@@ -0,0 +1,42 @@
+﻿module tests 
+
+    open NUnit.Framework
+    open FsUnit
+    open logic 
+
+    [<Test>]
+    let ``With op. Map -> Count of even num in list [-10..10] should be equal 11`` () =
+        evenNumWithMap [-10..10] |> should equal 11
+
+    [<Test>]
+    let ``With op. Map -> Count of even num in list [0..10] should be equal 6`` () =
+        evenNumWithMap [0..10] |> should equal 6
+
+    [<Test>]
+    let ``With op. Map -> Count of even num in empty list should be equal 0`` () =
+        evenNumWithMap [] |> should equal 0
+
+    [<Test>]
+    let ``With op. Filter -> Count of even num in list [-10..10] should be equal 11`` () =
+        evenNumWithFilter [-10..10] |> should equal 11
+
+    [<Test>]
+    let ``With op. Filter -> Count of even num in list [0..10] should be equal 6`` () =
+        evenNumWithFilter [0..10] |> should equal 6
+
+    [<Test>]
+    let ``With op. Filter -> Count of even num in empty list should be equal 0`` () =
+        evenNumWithFilter [] |> should equal 0
+
+    [<Test>]
+    let ``With op. Fold -> Count of even num in list [-10..10] should be equal 11`` () =
+        evenNumWithFold [-10..10] |> should equal 11
+
+    [<Test>]
+    let ``With op. Fold -> Count of even num in list [0..10] should be equal 6`` () =
+        evenNumWithFold [0..10] |> should equal 6
+
+    [<Test>]
+    let ``With op. Fold -> Count of even num in empty list should be equal 0`` () =
+        evenNumWithFold [] |> should equal 0
+            

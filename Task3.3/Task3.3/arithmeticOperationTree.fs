154adcdf2abcfd4c64a97883cc4d10869dc97926
Feodoros <fzhilkin@mail.ru>
2019-03-04 05:08:39 +0300

    Initial commit.

---
+++ Task3.3/Task3.3/arithmeticOperationTree.fs
@@ -0,0 +1,22 @@
+﻿module logic 
+    
+    //Match type of our phrase: it may be +, -, /, * or just a number.
+    type Phrase =
+        | Number of int
+        | Plus of Phrase * Phrase
+        | Minus of Phrase * Phrase
+        | Divide of Phrase * Phrase
+        | Multiply of Phrase * Phrase
+
+    //Now we'r looking on our tree and detect type of our phrase then make some arithmetic operations one by one, step by step, descending from our tree
+    let rec makeSomeOperation phrase =
+        match phrase with 
+        | Number number -> number
+        | Plus(phrase1, phrase2) -> (makeSomeOperation phrase1) + (makeSomeOperation phrase2)
+        | Minus(phrase1, phrase2) -> (makeSomeOperation phrase1) - (makeSomeOperation phrase2);
+        | Multiply(phrase1, phrase2) -> (makeSomeOperation phrase1) * (makeSomeOperation phrase2)
+        | Divide(phrase1, phrase2) -> 
+            try (makeSomeOperation phrase1) / (makeSomeOperation phrase2)
+            with
+                | Failure(msg) -> printfn "%s" msg; 0
+ 
---
+++ Task3.3/Task3.3_tests/arithmeticOperationTree_tests.fs
@@ -0,0 +1,17 @@
+﻿module tests 
+
+    open NUnit.Framework
+    open FsUnit
+    open logic 
+
+     [<Test>]
+    let ``Compute one num.`` () = 
+        makeSomeOperation <| Number(241) |> should equal 241
+
+    [<Test>]
+    let ``Сompute next: (2+2)*2.`` () = 
+        makeSomeOperation <| Multiply(Plus(Number 2, Number 2), Number 2) |> should equal 8
+
+    [<Test>]
+    let ``Compute next: 2*(600/3) - 161.`` () = 
+        makeSomeOperation <| Minus(Multiply(Number 2, Divide(Number 600, Number 3)), Number 161) |> should equal 239

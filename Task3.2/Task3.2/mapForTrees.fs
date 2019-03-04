9f185798c83d86f665c2cb54bb07e1d6aa0e06f4
Feodoros <fzhilkin@mail.ru>
2019-03-04 03:50:23 +0300

    Initial commit.

---
+++ Task3.2/Task3.2/mapForTrees.fs
@@ -0,0 +1,10 @@
+﻿module logic 
+
+    type Tree<'a> =
+        |Tree of 'a * Tree<'a> * Tree<'a>
+        |Tip of 'a
+
+    let rec mapFuncForBinTree someFunction tree =
+        match tree with
+        |Tree(node, leftChild, rightChild) -> Tree(someFunction node, mapFuncForBinTree someFunction leftChild, mapFuncForBinTree someFunction rightChild)
+        |Tip(node) -> Tip(someFunction node)

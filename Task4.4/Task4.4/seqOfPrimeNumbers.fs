46c8055a2f6bc3f30bbf88b9507db0680f201022
Feodoros <fzhilkin@mail.ru>
2019-03-05 23:38:00 +0300

    Initial commit.

---
+++ Task4.4/Task4.4/seqOfPrimeNumbers.fs
@@ -0,0 +1,20 @@
+﻿module logic 
+    
+    //Проверяем число: если у него нет делитель от 2 до квадратного корня из этого же числа, то оно простое.
+    let isPrime num = 
+        let limit = int (sqrt(float num))
+        seq{2..limit}
+        |> Seq.exists (fun x -> num % x = 0) 
+        |> not
+
+    //Не успокаиваемся пока не найдем след простое число.
+    let rec nextPrime n = 
+        if isPrime (n + 1) then n + 1
+        else nextPrime (n+1)
+
+    //Создаем последовательность с помощью Seq.unfold.
+    //Начинаем с 2 и следующее число в последовательности -  результат работы метода nextPrime,
+    //который берет крайний с права элемент последовательости
+    //и ищет следующее число (простое) в последовательность. 
+    let rec makeSequenceOfPrimes () =
+         Seq.unfold(fun n -> Some(n, nextPrime n)) 2
---
+++ Task4.4/Task4.4_tests/seqOfPrimeNumbers_tests.fs
@@ -0,0 +1,17 @@
+﻿module test
+
+    open NUnit.Framework
+    open FsUnit
+    open logic 
+
+     [<Test>]
+    let ``Check seq with prime num 13.`` () = 
+        Seq.exists (fun x -> x = 13) <| makeSequenceOfPrimes () |> should equal true
+
+    [<Test>]
+    let ``Check seq with prime num 239.`` () = 
+        Seq.exists (fun x -> x = 239) <| makeSequenceOfPrimes () |> should equal true
+
+    [<Test>]
+    let ``Let's see 42th prime number.`` () = 
+        makeSequenceOfPrimes () |> Seq.skip 41 |> Seq.take 1 |> Seq.toArray |> should equal [|181|]

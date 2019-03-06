c96aa680b59b748ab8c6dc6007df23be8c3e7b42
Feodoros <fzhilkin@mail.ru>
2019-03-06 20:10:24 +0300

    Add comments.

--- Task3.1/Task3.1/evenNumInList.fs
+++ Task3.1/Task3.1/evenNumInList.fs
@@ -1,10 +1,13 @@
 ﻿module logic  
 
+//Вычитаем из полного количества элементов количество нечетных элементов (нечет % 2 по модулю = 1, складываем эти единицы)
     let evenNumWithMap list = 
         List.length list - List.sum (List.map (fun x -> abs (x % 2)) list)
 
+//Четные берем в новый список и получаем его длину.
     let evenNumWithFilter list =
         List.length (List.filter (fun x -> x % 2 = 0) list)
 
+//Объявляем acc как длину исходного списка, далее вычитаем из нее единицу, когда встречаем нечет по аналогии с 1ым методом.
     let evenNumWithFold list =
         List.fold (fun acc x -> acc - abs (x % 2)) (List.length list) list

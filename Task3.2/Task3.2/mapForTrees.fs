7a79b3b7a10270bed84a9f333382f88d1b744363
Feodoros <fzhilkin@mail.ru>
2019-03-06 19:52:14 +0300

    Add comments.

--- Task3.2/Task3.2/mapForTrees.fs
+++ Task3.2/Task3.2/mapForTrees.fs
@@ -1,9 +1,11 @@
 ﻿module logic 
 
+	//Create tree
     type Tree<'a> =
         |Tree of 'a * Tree<'a> * Tree<'a>
         |Tip of 'a
-
+	
+	//Описываем функцию, которая будет применять определенную функцию к элементу (Tip) дерева либо, если дальше можем спуститься, то применяем функцию к корню и спускаемся вглубь. 
     let rec mapFuncForBinTree someFunction tree =
         match tree with
         |Tree(node, leftChild, rightChild) -> Tree(someFunction node, mapFuncForBinTree someFunction leftChild, mapFuncForBinTree someFunction rightChild)

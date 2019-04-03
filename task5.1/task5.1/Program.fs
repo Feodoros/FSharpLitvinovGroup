fa504d5f19161191a3b8bf42abca9670f53df88e
Feodoros <fzhilkin@mail.ru>
2019-04-04 01:48:46 +0300

    Initial commit.

---
+++ task5.1/task5.1/Program.fs
@@ -0,0 +1,24 @@
+﻿module logic 
+    
+    let checkBrackets (line : string)  = 
+
+        let brackets = ['('; ')'; '['; ']'; '{'; '}']
+
+        //Из строки делаем массив charов; из массива делаем список; фильтруем список, чтобы остались только скобки.
+        let charList = (List.filter(fun x -> List.contains(x) brackets) (Array.toList <| line.ToCharArray()))
+
+        //Строка только из скобок.
+        let mutable onlyBrackets = String.concat "" <| List.map string charList
+
+        //Удаляем из строки (),{},[] пока не выйдем из цикла. Если все ок, то результат - пустая строка.
+        let mutable n = - 1
+        let delete = 
+            while (onlyBrackets.Length <> n) do
+                n <- onlyBrackets.Length
+                onlyBrackets <- onlyBrackets.Replace("()","")
+                onlyBrackets <- onlyBrackets.Replace("{}","")
+                onlyBrackets <- onlyBrackets.Replace("[]","")
+
+        onlyBrackets = ""   
+
+    
---
+++ task5.1/task5.1_tests/Program.fs
@@ -0,0 +1,17 @@
+﻿module test
+
+    open NUnit.Framework
+    open FsUnit
+    open logic 
+
+     [<Test>]
+    let ``Check correctness 1 with correct string.`` () = 
+        checkBrackets "(string{[hello]})" |> should equal true
+
+    [<Test>]
+    let ``Check correctness 2 with correct string.`` () = 
+        checkBrackets "({[]} ({check this}))" |> should equal true
+
+    [<Test>]
+    let ``Check correctness 3 with wrong string.`` () = 
+        checkBrackets "{1[0[1]]" |> should equal false

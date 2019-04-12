381523df4a5430e8bd3b25460c723e04885ba693
Feodoros <fzhilkin@mail.ru>
2019-04-12 19:16:39 +0300

    test1.3

---
+++ test1.2/test1.2/Program.fs
@@ -0,0 +1,22 @@
+﻿module Logic = 
+
+    type Stack<'a>() =
+
+        let mutable stack = []
+
+        ///Push
+        member __.Push x = stack <- (x :: stack)
+
+        ///Pop
+        member __.Pop = 
+            match stack with
+            |h::t -> 
+                stack <- t
+                h
+            |[] -> failwith "Empty stack"
+
+        ///Empty
+        member __.IsEmpty = (List.isEmpty stack)
+
+        ///Lenght
+        member __.Length = stack.Length

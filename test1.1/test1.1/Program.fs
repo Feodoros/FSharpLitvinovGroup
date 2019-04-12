64d6dc2175ffb9673e407fc7bd68ffd1cefd0e02
Feodoros <fzhilkin@mail.ru>
2019-04-12 18:53:53 +0300

    Initial commit.

---
+++ test1.1/test1.1/Program.fs
@@ -0,0 +1,16 @@
+﻿module Logic 
+
+    ///По сути переписанная функция из первой домки. 
+    ///Индекс взят любой больше 40 (посмотрел список чисел на википедии). Я взял 42.
+    let sumNum () =
+        let rec loop counter secondPrevious  firstPrevious sum =               
+                let sumOfTwo = secondPrevious  + firstPrevious
+                let decreasedCounter = counter - 1
+                if (secondPrevious < 1000000) then
+                    if ((secondPrevious  % 2) = 0) then
+                        let helpingIncSum = sum + secondPrevious 
+                        loop decreasedCounter firstPrevious sumOfTwo helpingIncSum                    
+                    else 
+                        loop decreasedCounter firstPrevious sumOfTwo sum
+                else sum
+        loop 42 0 1 0 
---
+++ test1.1/test1.1_test/Program.fs
@@ -0,0 +1,9 @@
+﻿module Tests
+
+    open NUnit.Framework
+    open FsUnit
+    open Logic
+
+    [<Test>]
+    let ``Sum of even elements less than 1000000 in fib seq should be 1089154`` () =
+        sumNum ()|> should equal 1089154 

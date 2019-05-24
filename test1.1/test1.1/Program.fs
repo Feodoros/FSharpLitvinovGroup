e7003588ec689db27d9dd79aaf5ca7fb018c0e52
Feodoros <fzhilkin@mail.ru>
2019-05-24 14:37:49 +0300

    Prepare

--- test1.1/test1.1/Program.fs
+++ test1.1/test1.1/Program.fs
@@ -1,16 +1,15 @@
 ﻿module Logic 
+  
+    let supermap list functions =
+        let rec loop list func temp =
+            match list with
+            | [last] -> loop [] func (List.append(func last) temp)        
+            | h::t -> loop t func (List.append(func h) temp)
+            | [] -> temp  
+        loop list functions [] 
 
-    
-	let supermap list functions =
-		let rec loop list func temp =
-			match list with
-			| [last] -> loop [] func (List.append(func last) temp)        
-			| h::t -> loop t func (List.append(func h) temp)
-			| [] -> temp  
-		loop list functions [] 
+    let f x = x + 1
+    let g x = x + 3
 
-	let f x = x + 1
-	let g x = x + 3
-
-	let ans = supermap [1;2;3] (fun x -> [f; g])      
-	printf("%A") ans  
+    let ans = supermap [1;2;3] (fun x -> [f x; g x])      
+    printf("%A") ans  
--- test1.1/test1.1_test/Program.fs
+++ test1.1/test1.1_test/Program.fs
@@ -5,5 +5,7 @@
     open Logic
 
     [<Test>]
-    let ``Test ``Test list```` () =
-        supermap [1;2;3] (fun x -> [x + 1; x + 3])  ()|> should equal [4; 6; 3; 5; 2; 4] 
+    let ``Test list`` () =
+        let f x = x + 1
+        let g x = x + 3
+        supermap [1;2;3] (fun x -> [f x; g x]) |> should equal [4; 6; 3; 5; 2; 4] 

049e3a59824daffae626cb3e3ffb8fa53561c225
Feodoros <fzhilkin@mail.ru>
2019-05-28 17:02:45 +0300

    28.05

--- test2/test2/Program.fs
+++ test2/test2/Program.fs
@@ -1,6 +1,53 @@
-﻿module Logic 
+﻿module Logic
 
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    // Тип для хранения легкового автомобиля.
+    type LightCar(model: string, color: string, brend: string, value: int) =
+        member this.model = model
+        member this.color = color
+        member this.brend = brend
+        member this.value = value
+
+    // Тип для хранения грузового автомобиля.
+    type HeavyCar(weight: int, brend: string, value: int) =
+        member this.weight = weight
+        member this.brend = brend
+        member this.value = value
+    
+    // Тип для хранения информации об автомобилях.
+    type AutoShowInfo(lightCars: list<LightCar>, heavyCars: list<HeavyCar>) =
+        member this.lightCars = lightCars
+        member this.heavyCars = heavyCars
+
+        // Общая сумма всех автомобилей.
+        member this.getSummaryValue() =
+
+            // Общая сумма легковых автомобилей.
+            let rec loop1 (list: list<LightCar>, acc) = 
+                match list with
+                |head::tail -> loop1 (tail, (acc + head.value))
+                |[] -> acc
+
+            // Общая сумма грузовых автомобилей.
+            let rec loop2 (list: list<HeavyCar>, acc) = 
+                match list with
+                |head::tail -> loop2 (tail, (acc + head.value))
+                |[] -> acc
+
+            (loop1 (lightCars, 0) + loop2 (heavyCars, 0))
+
+        // Все марки продаваемых автомобилей.
+        member this.getAllBrends() =
+
+            // Все марки легковых автомобилей.
+            let rec loop1 (list: list<LightCar>, acc: list<string>) = 
+                match list with
+                |head::tail -> loop1 (tail, (head.brend :: acc))
+                |[] -> acc
+
+            // Все марки грузовых автомобилей.
+            let rec loop2 (list: list<HeavyCar>, acc) = 
+                match list with
+                |head::tail -> loop2 (tail, (head.brend :: acc))
+                |[] -> acc
+
+            (loop1 (lightCars, [])) @ (loop2 (heavyCars, [])) |> List.distinct
--- test2/test2_tests/Program.fs
+++ test2/test2_tests/Program.fs
@@ -1,9 +1,43 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿module Tests
 
-open System
-open Logic
+    open NUnit.Framework
+    open FsUnit
+    open Logic
 
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+
+    [<Test>]
+    let ``GetSummaryValue`` () =
+        let lightCar1 = LightCar("Explorer", "White", "Ford", 2500000)
+        let lightCar2 = LightCar("T", "Black", "Tesla", 8000000)
+        let lightCar3 = LightCar("Hover H3", "Gray", "GREAT WALL", 1100000)
+
+        let lightCars = [lightCar1; lightCar2; lightCar3]
+
+        let heavyCar1 = HeavyCar(7000, "Ford", 5000000)
+        let heavyCar2 = HeavyCar(10000, "Hyuindai", 3000000)
+        let heavyCar3 = HeavyCar(7000, "BMW", 20000000)
+
+        let heavyCars = [heavyCar1; heavyCar2; heavyCar3]
+
+        let autoShowInfo = AutoShowInfo(lightCars, heavyCars)
+ 
+        autoShowInfo.getSummaryValue() |> should equal 39600000
+
+
+    [<Test>]
+    let ``getAllBrends`` () =
+        let lightCar1 = LightCar("Explorer", "White", "Ford", 2500000)
+        let lightCar2 = LightCar("T", "Black", "Tesla", 8000000)
+        let lightCar3 = LightCar("Hover H3", "Gray", "GREAT WALL", 1100000)
+
+        let lightCars = [lightCar1; lightCar2; lightCar3]
+
+        let heavyCar1 = HeavyCar(7000, "Ford", 5000000)
+        let heavyCar2 = HeavyCar(10000, "Hyuindai", 3000000)
+        let heavyCar3 = HeavyCar(7000, "BMW", 20000000)
+
+        let heavyCars = [heavyCar1; heavyCar2; heavyCar3]
+
+        let autoShowInfo = AutoShowInfo(lightCars, heavyCars)
+
+        autoShowInfo.getAllBrends() |> should equal ["GREAT WALL"; "Tesla"; "Ford"; "BMW"; "Hyuindai"]

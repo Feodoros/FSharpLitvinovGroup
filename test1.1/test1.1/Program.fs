dc3af5897c0dc6b482539aed7b11ccf4987a7084
Feodoros <fzhilkin@mail.ru>
2019-05-28 16:30:52 +0300

    28.05

--- test1.1/test1.1/Program.fs
+++ test1.1/test1.1/Program.fs
@@ -1,15 +1,11 @@
 ﻿module Logic 
   
-    let supermap list functions =
-        let rec loop list func temp =
-            match list with
-            | [last] -> loop [] func (List.append(func last) temp)        
-            | h::t -> loop t func (List.append(func h) temp)
-            | [] -> temp  
-        loop list functions [] 
+    let makeseq x = Seq.take(x) <| Seq.initInfinite(fun n -> x)
+    let make () =
+        let rec seqTask seq n = 
+            let seq1 = Seq.append seq <| makeseq (n) 
+            seqTask seq1 (n + 1) 
+        seqTask (Seq.empty<int>) 1
 
-    let f x = x + 1
-    let g x = x + 3
 
-    let ans = supermap [1;2;3] (fun x -> [f x; g x])      
-    printf("%A") ans  
+    printfn "%A" (make())  

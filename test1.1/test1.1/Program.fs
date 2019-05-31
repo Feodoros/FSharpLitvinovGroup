038a0e9443d8f57b8c95afbc7e5feb64a0ce5809
Feodoros <fzhilkin@mail.ru>
2019-05-31 16:12:02 +0300

    31.05

--- test1.1/test1.1/Program.fs
+++ test1.1/test1.1/Program.fs
@@ -1,11 +1,15 @@
 ﻿module Logic 
-  
-    let makeseq x = Seq.take(x) <| Seq.initInfinite(fun n -> x)
-    let make () =
-        let rec seqTask seq n = 
-            let seq1 = Seq.append seq <| makeseq (n) 
-            seqTask seq1 (n + 1) 
-        seqTask (Seq.empty<int>) 1
 
+    open System
+    let avgOfSin list =
+        let rec loop list acc n =
+            match list with
+            | [] -> acc
+            | head::tail -> 
+                            let sum = acc + Math.Sin(head)/(float(List.length list + n)) 
+                            let counter = n + 1                       
+                            loop tail sum counter
+        loop list 0.0 0
 
-    printfn "%A" (make())  
+    let ans = Math.Round(avgOfSin [Math.Asin(1.0); Math.Asin(Math.PI/4.0); Math.Asin(Math.PI/8.0)], 3)
+    printfn "%A" ans
--- test1.1/test1.1_test/Program.fs
+++ test1.1/test1.1_test/Program.fs
@@ -3,9 +3,16 @@
     open NUnit.Framework
     open FsUnit
     open Logic
+    open System
 
     [<Test>]
-    let ``Test list`` () =
-        let f x = x + 1
-        let g x = x + 3
-        supermap [1;2;3] (fun x -> [f x; g x]) |> should equal [4; 6; 3; 5; 2; 4] 
+    let ``Test 1`` () =       
+         Math.Round(avgOfSin [Math.Asin(1.0); Math.Asin(Math.PI/4.0); Math.Asin(Math.PI/8.0)], 3)   
+         |> should equal 0.726
+        
+    [<Test>]
+    let ``Test 2`` () = 
+         let delta = 0.02  
+         let avg = avgOfSin [1.0; 2.0; 3.0; 4.0]
+         let ans = ((avg - delta) < avg && avg < (avg + delta))
+         ans |> should equal true

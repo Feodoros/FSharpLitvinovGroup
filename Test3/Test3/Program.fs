6382feb13581e98d8bd92c7fb5f592381ab48912
Feodoros <fzhilkin@mail.ru>
2019-05-31 17:32:24 +0300

    Initial commit

---
+++ Test3/Test3/Program.fs
@@ -0,0 +1,24 @@
+﻿module Logic 
+    open System.Collections
+    open System.Threading
+
+    type LockQueue() =
+
+        let mutable que = Queue()        
+
+        let event = new AutoResetEvent(false)
+
+        member this.IsEmpty =
+            lock this (fun _ -> que.Count = 0)
+
+        member this.Enqueue x =
+            lock this (fun _ -> if (Interlocked.CompareExchange(ref que.Count, 0, 0) = 0) then 
+                                    event.Set() |> ignore 
+                                    que.Enqueue(x))
+
+        member this.Dequeue =
+            if (Interlocked.CompareExchange(ref que.Count, 0, 0) = 0) then 
+                event.WaitOne() |> ignore
+                que.Dequeue()
+            else 
+                lock this (fun _ -> que.Dequeue())
---
+++ Test3/Test3_tests/Program.fs
@@ -0,0 +1,47 @@
+﻿module Tests
+
+    open NUnit.Framework
+    open FsUnit
+    open Logic
+
+    ///В тестах тестируем сразу Dequeue, Enqueue, IsEmpty
+    [<Test>]
+    let ``Test 1`` () =       
+        let que = LockQueue()
+
+        let testLoop = 
+            async {
+                for i in [1..100] do
+                    que.Enqueue i                                           
+                do! Async.Sleep 10  
+                printfn "..after"
+                }
+        Async.RunSynchronously testLoop
+        que.IsEmpty |> should equal false
+
+    [<Test>]
+    let ``Test 2`` () =       
+        let que = LockQueue()
+
+        let enqueue = async {
+            que.Enqueue 1
+            do! Async.Sleep 2
+        }
+
+        let dequeue = async {
+            let que1 = que.Dequeue 
+            do! Async.Sleep 2
+            que1 |> ignore
+        }
+
+        let enq = enqueue
+        let deq = dequeue
+
+        [enq; deq] 
+        |> Async.Parallel
+        |> Async.RunSynchronously  |> ignore
+
+        que.IsEmpty |> should equal true    
+        
+        
+   

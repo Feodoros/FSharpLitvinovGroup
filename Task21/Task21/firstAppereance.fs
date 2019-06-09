a62683922158b377bad267c62dee7d7fcb7d4fdb
Feodoros <fzhilkin@mail.ru>
2019-06-09 18:25:19 +0300

    Update

--- Task21/Task21/firstAppereance.fs
+++
@@ -1,5 +0,0 @@
-﻿    let rec fibs = Seq.append [0;1] (Seq.delay (fun () -> Seq.map2 (+) fibs (Seq.skip 1 fibs))) 
-    let list = Seq.toList fibs 
-    let func = List.sum (List.filter (fun x -> x % 2 = 0) (List.filter(fun x -> x < 1000000) list))
-
-printf "%A" func

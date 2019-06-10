26d0ee4a3ec60ab0d46d1af3997c6bec9111fbcb
Feodoros <fzhilkin@mail.ru>
2019-06-10 13:05:12 +0300

    Initial commit

---
+++ task7.1/task7.1_test/Program.fs
@@ -0,0 +1,27 @@
+﻿module Tests
+
+    open NUnit.Framework
+    open FsUnit
+    open Logic
+
+    [<Test>]
+    let ``Test WorkFlow 1``() = 
+
+        let l = MathFlow 3 {
+                let! a = 2.0 / 12.0
+                let! b = 3.5
+                return a / b
+            }
+
+        l |> should equal 0.048
+
+    [<Test>]
+    let ``Test WorkFlow 2``() = 
+
+        let l = MathFlow 2 {
+                let! a = 4.0 / 12.0
+                let! b = 3.0
+                return a / b
+            }
+
+        l |> should equal 0.11

8da5926d70e6e582828f5fa4d54aef183075ce0d
Feodoros <fzhilkin@mail.ru>
2019-10-08 14:58:32 +0300

    Remade 2.0
    Rewrite programm without mutable

--- task5.3/task5.3/Program.fs
+++ task5.3/task5.3/Program.fs
@@ -1,115 +1,153 @@
 ﻿module Logic 
-    open System.IO
-    open System
-    open System.Collections.Generic
     
-    type User =
-        {
-            userName : string; 
-            userNumber : string;             
-        }
-
-    type Book () = 
-
-        let list = List<User>()  
-
-        member Book.GetUserList = list
-
-        member Book.Add (user : User) = list.Add(user)
-
-        member Book.SearchNumber (name : string) =             
-            list.Find(fun i -> i.userName = name).userNumber
-               
-        member Book.SearchName (number : string) =
-            list.Find(fun i -> i.userNumber = number).userName
-                
-        member Book.Print (list : List<User>) =             
-            list.ForEach(fun i -> printfn " Name: %A \n Phone: %A \n" i.userName i.userNumber )         
-
-        member Book.CreateFile (list : List<User>) = 
-
-            let rec reformList (list: List<User>) (list1: List<String>) = 
-                if list.Count = 0 then list1
-                else list1.Add(list.Item(0).userName + " " + list.Item(0).userNumber)
-                     list.RemoveAt(0)  
-                     reformList list list1
-                
-            let listx = List<String>()    
-            let fileFromBook = (reformList list listx).ToArray()  
-            File.WriteAllLines("handbook.txt", fileFromBook)   
-
-        member Book.ReadData newlist  =                 
-                let rec readDataFromFile (contactList : List<User>) listFromTextFileBook =
-                    match listFromTextFileBook with
-                    |[] -> contactList
-                    | (h : string) :: (t : string list) -> 
-                        let textBook = Seq.toList (h.Split (' ', '\n'))  
-                        let name = List.head textBook 
-                        let number = List.head (List.tail textBook)
-                        let user = {userName = name; userNumber = number }
-                        contactList.Add(user)
-                        readDataFromFile contactList t
-                list.Clear() 
-                list.AddRange(readDataFromFile newlist (Seq.toList (File.ReadLines("handbook.txt"))))
+open System.IO
+open System
+open System.Runtime.Serialization.Formatters.Binary
+open System.Collections.Generic
         
-    let contactList = List<User>()    
-    let book = Book()
-    ///Запускаем телефонный справочник в интерактивном режиме.
-    let rec loop contactList =            
-        printfn "1 -> Exit"
-        printfn "2 -> New contact"
-        printfn "3 -> Search number"
-        printfn "4 -> Search name"
-        printfn "5 -> See all contacts"
-        printfn "6 -> New text file"
-        printfn "7 -> Read text file"
+/// Класс Book сделан для тестирования
+type Book () =
+    /// key -- number, value -- name, так как номер должен быть уникальным -- 
+    /// у одного пользователя может быть несколько номеров, но у одного номера только один пользователь
+    member Book.Add (contactList, name, number) =
+        Map.add number name contactList 
+
+    member Book.FindByName (contactList, name) = 
+      
+        let rec takeOne dList list = 
+            match dList with
+            | head::tail -> takeOne tail ((fst head)::list)
+            |_ -> list
+        takeOne (Map.toList <| Map.filter (fun key value -> value = name) contactList) List.Empty
+
+
+    member Book.FindByNumber (contactList, number) = 
+        let rec takeOne dList list = 
+            match dList with
+            | head::tail -> takeOne tail ((snd head)::list)
+            |_ -> list
+        takeOne (Map.toList <| Map.filter (fun key value -> key = number) contactList) List.Empty
+
+    member Book.WriteFile (contactList) = 
+        let writeValue outputStream (x: 'a) =
+                let bf = BinaryFormatter()
+                bf.Serialize(outputStream, box x)
+
+        use fsOut = new FileStream("handbook.txt", FileMode.Create)
+        writeValue fsOut contactList
+        fsOut.Close()           
+
+    /// Добавляем в книгу только те конакты из текстового файла, номера которых
+    /// не встречаются в уже имеющейся книге
+    member Book.ReadFile (contactList) = 
+
+        let rec removeCopy map1 map2 = 
+            match map1 with
+            | head::tail -> let newMap = Map.filter (fun key value -> key <> (fst head)) map2
+                            removeCopy tail newMap 
+                            
+            |_ -> map2 
+
+        let toTuple (kvp : KeyValuePair<_,_>) =
+            kvp.Key, kvp.Value
+
+        /// Concatenate all the input maps. Where there are duplicate input keys, 
+        /// the last mapping is preserved.
+        let concat maps = 
+            maps
+            |> Seq.concat
+            |> Seq.map toTuple
+            |> Map.ofSeq
+
+        let readValue inputStream =
+            let bf = BinaryFormatter()
+            let res = bf.Deserialize(inputStream)
+            unbox res
+
+        try
+            use fsIn = new FileStream("handbook.txt", FileMode.Open)
+            let res : Map<string, string> = readValue fsIn            
+            fsIn.Close()
+            let newRes = removeCopy (Map.toList contactList) res
+            concat [|newRes; contactList|] 
+        with
+            | :? System.IO.FileNotFoundException -> 
+                                                    printfn "Had not found file!";
+                                                    contactList
+
+let book = Book()
+let contactList = Map.empty
+
+///Запускаем телефонный справочник в интерактивном режиме.
+let rec loop contactList =            
+    printfn "1 -> Exit"
+    printfn "2 -> New contact"
+    printfn "3 -> Search number"
+    printfn "4 -> Search name"
+    printfn "5 -> See all contacts"
+    printfn "6 -> New text file"
+    printfn "7 -> Read text file"
+    
+    let x = System.Console.ReadLine()
+    match x with
+    |"1" -> 
+        printfn "You've left handbook."
+        Environment.Exit(0)
+
+    ///Добавляем только уникальные номера и имена в телефонный справочник.    
+    |"2" -> 
+        printfn "Name: "
+        let name = Console.ReadLine()   
+              
+        printfn "Number: "
+        let number = Console.ReadLine()
+
+        if (Map.exists (fun key value -> key = number) contactList) then
+            printfn "This name has already used."  
+             
+        loop <| book.Add(contactList, name, number)                
+
+    |"3" ->
+         printfn "Write name: "                 
+         let name = Console.ReadLine()      
+         
+         if  book.FindByName(contactList, name).IsEmpty then 
+            printfn "This name is not used"
+         else
+            printfn "Found numbers: "
+            List.iter (fun i -> printfn "%A" i ) <| book.FindByName(contactList, name)
+
+         loop contactList
+
+    |"4" -> 
+        printfn "Write number: "
+        let number = Console.ReadLine()
+
+        if  book.FindByNumber(contactList, number).IsEmpty then 
+            printfn "This number is not used"
+        else
+            printfn "Found name: "
+            List.iter (fun i -> printfn "%A" i ) <| book.FindByNumber(contactList, number)
+
+        loop contactList
+
+    |"5" -> 
+        if (contactList.IsEmpty) then printfn "Add something to handbook."
+        else Map.iter (fun key value -> printfn " Name: %A \n Phone: %A \n" value key) <| contactList               
+        loop contactList
+
+    |"6" -> 
+        book.WriteFile(contactList)
+        printfn("Successfully writed data.")
+        loop contactList
         
-        let x = System.Console.ReadLine()
-        match x with
-        |"1" -> 
-            printfn "You've left handbook."
-        ///Добавляем только уникальные номера и имена в телефонный справочник.    
-        |"2" -> 
-            printfn "Name: "
-            let name = Console.ReadLine()              
-            if (book.GetUserList.Exists (fun i -> i.userName = name)) then 
-                printfn "This name has already used."
-            else               
-                printfn "Number: "
-                let number = Console.ReadLine()
-                if (book.GetUserList.Exists(fun i -> i.userNumber = number)) then 
-                    printfn "This number has already used." 
-                else                   
-                    let user = {userName = name; userNumber = number}            
-                    book.Add(user)
-            loop book.GetUserList
-        |"3" ->
-             printfn "Write name: "                 
-             let name = Console.ReadLine()               
-             printfn "%s" <| book.SearchNumber(name)
-             loop contactList
-        |"4" -> 
-            printfn "Write number: "
-            let number = Console.ReadLine()
-            printfn "%s" <| book.SearchName(number) 
-            loop contactList
-        |"5" -> 
-            if (contactList.Count = 0) then printfn "Add something to handbook."
-            else book.Print contactList                
-            loop contactList
-        |"6" -> 
-            book.CreateFile(contactList)
-            printfn "Successfully."
-            loop contactList
-        |"7" -> 
-            if (File.Exists("handbook.txt")) then         
-                book.ReadData contactList
-                printfn "Successfully" 
-                loop (book.GetUserList)
-            else 
-                printfn "Nothing found." 
-                loop contactList
-        |_ -> 
-            printfn "This command is not defined." 
-            loop contactList
+    |"7" -> 
+        printfn "Successfully loaded phonebook!"
+        loop <| book.ReadFile(contactList)            
             
+    |_ -> 
+        printfn "This command is not defined." 
+        loop contactList
+
+loop contactList
+
--- task5.3/task5.3_tests/Program.fs
+++ task5.3/task5.3_tests/Program.fs
@@ -3,55 +3,59 @@
     open NUnit.Framework
     open FsUnit
     open Logic 
-    open System.Linq
 
     [<Test>]
     let ``Check adding to book.`` () =
-
-        let contact'0 = {userName = "Fedor"; userNumber = "777"}
-        let contact'1 = {userName = "Alex"; userNumber = "665"}
+        
+        let emptyList = Map.empty
+        let contactList = emptyList.Add("322", "Fedor").Add("321", "Ilya").Add("123", "Alex")
 
         let book = Book()
-        book.Add(contact'0)
-        book.Add(contact'1)
+        
+        let updatedList = book.Add(book.Add(contactList, "SomeOne", "322"), "Roman", "998") 
 
-        (book.GetUserList).Exists (fun i -> i.userName = contact'0.userName && i.userNumber = contact'0.userNumber) 
-        |> should equal true
+        let check1 = Map.filter(fun key value -> key = "998" && value = "Roman") updatedList
+        let check2 = Map.filter(fun key value -> key = "322" && value = "Fedor") updatedList
 
-        (book.GetUserList).Exists(fun i -> i.userName = contact'1.userName && i.userNumber = contact'1.userNumber) 
-        |> should equal true
+        check1 = Map.empty |> should equal false       
+        check2 = Map.empty |> should equal true
    
     [<Test>]
     let ``Search number by name and search name by number.`` () =
-        let contact'0 = {userName = "Fedor"; userNumber = "777"}
-        let contact'1 = {userName = "Alex"; userNumber = "665"}
-
-        let book = Book()
-        book.Add(contact'0)
-        book.Add(contact'1)
-
-        book.SearchNumber("Alex") 
-        |> should equal "665"
-
-        book.SearchName("777") 
-        |> should equal "Fedor"
         
+        let book = Book()
+
+        let emptyList = Map.empty
+        let contactList = emptyList.Add("322", "Fedor").Add("321", "Fedor").Add("123", "Alex")
+        
+        let foundNames = book.FindByNumber(contactList, "123")
+        let foundNumbers = book.FindByName(contactList, "Fedor")
+
+        let check = List.exists(fun i -> i = "321") <| foundNumbers && 
+                    List.exists(fun i -> i = "322") <| foundNumbers &&
+                    List.length foundNumbers = 2                    &&
+                    List.length foundNames = 1                      &&
+                    List.exists(fun i -> i = "Alex") <| foundNames
+        check |> should equal true        
     
     [<Test>]
     let ``Check read and write data.`` () =
-        let contact'0 = {userName = "Fedor"; userNumber = "777"}
-        let contact'1 = {userName = "Alex"; userNumber = "665"}
-
+        
         let book = Book()
-        book.Add(contact'0)
-        book.Add(contact'1)
 
-        book.CreateFile (book.GetUserList)
+        let emptyList = Map.empty
+        let contactList1 = emptyList.Add("322", "Fedor").Add("123", "Alex")
 
-        let book1 = Book()
-        book1.ReadData (book.GetUserList)
 
-        book.GetUserList.SequenceEqual(book1.GetUserList)  
-        |> should equal true
+        book.WriteFile(contactList1)
+
+        let contactList2 =  book.ReadFile(Map.empty)       
+        let check = List.exists(fun i -> i = ("322", "Fedor")) <| Map.toList contactList2 &&
+                    List.exists(fun i -> i = ("123", "Alex")) <| Map.toList contactList2  &&
+                    List.length <| Map.toList contactList2 = 2
+        check |> should equal true
+
+                                                                            
+      
 
         
